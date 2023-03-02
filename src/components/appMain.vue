d42963ba8d3cc2ac33351b3c5cd633e1d0c61d29
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-03-02 18:17:59 +0100

    add: chiamata axios in appMain

--- src/components/appMain.vue
+++ src/components/appMain.vue
@@ -1,10 +1,41 @@
 <script>
 import axios from 'axios';
+
+export default{
+    name: 'appMain',
+    components : {
+      
+    },
+    data() {
+        return {
+            projectList:[],
+            url: 'http://127.0.0.1:8000/api/projects',
+        }
+    },
+    methods: {
+        getProjects(){
+            axios.get(this.url,{
+                params:{
+                   
+                }
+            })
+            .then((response) => {
+                console.log(response.data.results);
+                this.projectList = response.data.results;
+            })
+        }
+    },
+    created() {
+        this.getProjects();
+    }
+}
 </script>
 
 <template>
-    <div>
-        ciao
+    <div class="container">
+        <div class="" v-for="element in projectList">
+            <p>{{ element.title }}</p>
+        </div>
     </div>
   
 </template>
