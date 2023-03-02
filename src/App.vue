62907ac6ce0d82756f7ee1d6c4220889eb7ec7d2
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-03-02 17:49:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import appMain from './components/appMain.vue';
+
+export default {
+  name: 'AppVue',
+  components: {
+    appMain,
+  }
+}
+</script>
+
+<template>
+  <appMain/>
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as * ;
+@use "bootstrap/scss/bootstrap" as *;
+
+
+</style>
---
+++ src/components/appMain.vue
@@ -0,0 +1,17 @@
+<script>
+import axios from 'axios';
+</script>
+
+<template>
+    <div>
+        ciao
+    </div>
+  
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as * ;
+@use '../styles/partials/variables' as * ;
+
+
+</style>
