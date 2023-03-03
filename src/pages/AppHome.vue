bc2d7b1629cb0a4d8cf45feafca3f35ce1b04d83
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-03-03 15:26:20 +0100

    create: pages

---
+++ src/pages/AppHome.vue
@@ -0,0 +1,23 @@
+<script>
+
+
+export default {
+  name: 'AppHome',
+  components: {
+    
+  }
+}
+</script>
+
+<template>
+
+ 
+
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as * ;
+
+
+
+</style>
---
+++ src/pages/AppProjects.vue
@@ -0,0 +1,23 @@
+<script>
+
+
+export default {
+  name: 'AppProjects',
+  components: {
+    
+  }
+}
+</script>
+
+<template>
+
+ 
+
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as * ;
+
+
+
+</style>
