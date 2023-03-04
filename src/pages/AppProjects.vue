40cc69ca5530751b67d912b7096a6b6c57713592
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-03-04 14:58:07 +0100

    add: chiamata api in AppProjects

--- src/pages/AppProjects.vue
+++ src/pages/AppProjects.vue
@@ -1,23 +1,48 @@
 <script>
+import axios from 'axios';
+import AppProject from '../components/AppProject.vue';
 
+export default{
+    name: 'AppProjects',
+    components : {
+        AppProject,
+      
+    },
+    data() {
+        return {
+            projectList:[],
+            url: 'http://127.0.0.1:8000/api/projects',
+        }
+    },
+    methods: {
+        getProjects(){
+            axios.get(this.url,{
+                params:{
+                   
+                }
+            })
+            .then((response) => {
+                this.projectList = response.data.results;
+            })
 
-export default {
-  name: 'AppProjects',
-  components: {
-    
-  }
+            .catch(function (error) {
+                console.log(error);
+            })
+        }
+    },
+    created() {
+        this.getProjects();
+    }
 }
 </script>
 
 <template>
-
- 
-
+    <AppProject v-for="project in projectList" :project="project"/>
 </template>
 
-<style lang="scss">
-@use './styles/general.scss' as * ;
-
+<style lang="scss" scoped>
+@use '../styles/general.scss' as * ;
+@use '../styles/partials/variables' as * ;
 
 
 </style>
