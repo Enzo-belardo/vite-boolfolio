b702528b7f074b5fd948b0b1acb5f4bdd128b6cb
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-03-05 15:03:37 +0100

    implemented singleProject

--- src/components/AppProject.vue
+++ src/components/AppProject.vue
@@ -15,19 +15,21 @@ export default{
         'project': {
             type: Object,
             required: true,
-        },
+        },  
     },
 }
 </script>
 
 <template>
-    <div class=" col-xs-10 col-sm-10 col-md-6 col-lg-4 .col-xl-4">
+    <div class="col-xs-10 col-sm-10 col-md-6 col-lg-4 .col-xl-4">
         <div class="card"> 
             <img :src="project.image" class="img-fluid" :alt="project.title">
             <div class="card-body" >
                 <h5 class="card-title">{{ project.title }}</h5>
-                <p class="card-text">{{ project.description.substr(0,150) }}</p>
-                <a href="#" class="btn btn-primary">view</a>
+                <p class="card-text">{{ project.description.substr(0, 140) }}</p>
+                <router-link :to="{name: 'singleProject'}" class="btn btn-primary">
+                    view
+                </router-link>
             </div>
         </div>
     </div>
---
+++ src/pages/AppSingleProject.vue
@@ -0,0 +1,55 @@
+<script>
+import axios from 'axios';
+import AppProject from '../components/AppProject.vue';
+
+export default{
+    name: 'AppSinglePost',
+    components : {
+        AppProject,
+      
+    },
+    data() {
+        return {
+            project: null,
+            url: 'http://127.0.0.1:8000/api/projects',
+        }
+    },
+    methods: {
+        getProject(){
+            axios.get(this.url,{
+                params:{
+                   
+                }
+            })
+            .then((response) => {
+                this.project = response.data.results;
+            })
+
+            .catch(function (error) {
+                console.log(error);
+            })
+        }
+    },
+    created() {
+        this.getProject();
+    }
+}
+</script>
+
+<template>
+    
+
+    <div class="container d-flex flex-wrap p-3">
+        <div class="row g-3">
+            <AppProject :project="project"/>     
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as * ;
+@use '../styles/partials/variables' as * ;
+
+
+
+</style>
