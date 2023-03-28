87f2509e143fc8a84985a566044757fe8ab0bd78
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-03-28 20:43:00 +0200

    add: contacts e project route

--- src/router.js
+++ src/router.js
@@ -1,8 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import Home from './pages/AppHome.vue';
+import Home from './pages/AppHome.vue'
 import Projects from './pages/AppProjects.vue';
 import AboutUs from './pages/AppAbout.vue';
+import SingleProject from './pages/AppSingleProject.vue';
+import contactUs from './pages/AppContacts.vue';
+
 
 
 const router = createRouter({
@@ -13,15 +16,29 @@ const router = createRouter({
             name: 'home',
             component: Home
         },
+        
         {
             path: '/projects',
             name: 'projects',
             component: Projects
         },
+
         {
             path: '/about',
             name: 'about',
             component: AboutUs
+        },
+
+        {
+            path: '/projects/:id',
+            name: 'project',
+            component: SingleProject
+        },
+
+        {
+            path: '/contacts',
+            name: 'contactUs',
+            component: contactUs
         },
     ]
 });
