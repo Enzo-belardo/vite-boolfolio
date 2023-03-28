444c2979cf679910dd53a0853ebab14b32a5fe4e
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-03-28 20:41:23 +0200

    add: props

--- src/components/AppProject.vue
+++ src/components/AppProject.vue
@@ -1,4 +1,6 @@
 <script>
+
+
 export default{
     name: 'AppProject',
     components : {
@@ -14,24 +16,36 @@ export default{
     {
         'project': {
             type: Object,
-            required: true,
+            required: false,
+            default: null
         },  
-    },
+        
+        'isShow': {
+            type: Boolean,
+            required: false,
+            default: false
+        },
+
+        'isHover': {
+          type: Boolean,
+          default: true
+       },
+    }
 }
 </script>
 
 <template>
-    <div class="col-xs-10 col-sm-10 col-md-6 col-lg-4 .col-xl-4">
-        <div class="card"> 
-            <img :src="project.image" class="img-fluid" :alt="project.title">
-            <div class="card-body" >
+    <div class="col-xs-10 col-sm-10 col-md-6 col-lg-4 col-xl-4" :class=" isShow ? ' col-xl-10 col-lg-10 col-md-10 col-sm-10' : ''">
+        <div class="card" :class="{ 'no-hover': !isHover }" > 
+            <img :src="project ? project.image : ''" class="img-fluid" :alt="project ? project.title : ''">
+            <div class="card-body" v-if="project">
                 <h5 class="card-title">{{ project.title }}</h5>
-                <p class="card-text">{{ project.description.substr(0, 140) }}</p>
-                <router-link :to="{name: 'singleProject'}" class="btn btn-primary">
-                    view
+                <p class="card-text"> {{ (isShow) ? project.description : project.description.substr(0, 140) }}</p>
+                <router-link :to="{name: 'project' , params:{ id: project.id }}" :class="this.$route.name === 'project' ? 'd-none' : 'd-inline'" class="btn btn-primary">
+                    Guarda
                 </router-link>
             </div>
-        </div>
+        </div>  
     </div>
 </template>
 
@@ -39,5 +53,23 @@ export default{
 @use '../styles/general.scss' as * ;
 @use '../styles/partials/variables' as * ;
 
+.card-body{
+    background-color: $primary-color;
+    color: $secondary-color;
+}
+.card {
+  border: none;
+  box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.3);
+  transition: all 0.3s ease;
 
+  &:hover {
+    box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.3);
+    transform: translateY(-5px);
+  }
+  &.no-hover:hover {
+    box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.3);
+    transform: none;
+  }
+
+}
 </style>
