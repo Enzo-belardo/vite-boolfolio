edcffcd909e6ef9676344294bcaee3a74599e398
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-03-28 20:43:55 +0200

    create: appFooter and appContacts

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,22 @@
+<script>
+
+
+
+
+export default {
+  name: 'AppFooter',
+  components: {
+  }
+}
+</script>
+
+<template>
+
+
+</template>
+
+<style lang="scss">
+
+
+
+</style>
---
+++ src/pages/AppContacts.vue
@@ -0,0 +1,26 @@
+<script>
+
+
+export default {
+  name: 'AppContacts',
+  components: {
+    
+  }
+}
+</script>
+
+<template>
+
+    <div>
+        <h1>
+            WORK IN PROGRESS
+        </h1>
+    </div>
+  
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/variables' as * ;
+</style>
+
+
