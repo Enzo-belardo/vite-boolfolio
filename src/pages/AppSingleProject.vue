ea59972dcd4207541107708bdc2f8e72cd944de0
Enzo-belardo <vincenzobelardo10@gmail.com>
2023-03-28 20:41:50 +0200

    fix: call api

--- src/pages/AppSingleProject.vue
+++ src/pages/AppSingleProject.vue
@@ -1,55 +1,48 @@
 <script>
-import axios from 'axios';
 import AppProject from '../components/AppProject.vue';
+import axios from 'axios';
 
-export default{
-    name: 'AppSinglePost',
-    components : {
-        AppProject,
-      
+export default {
+    name: 'AppSingleProject',
+    components: {
+        AppProject
     },
     data() {
         return {
             project: null,
-            url: 'http://127.0.0.1:8000/api/projects',
+            urlAddress: 'http://127.0.0.1:8000',
         }
     },
     methods: {
-        getProject(){
-            axios.get(this.url,{
-                params:{
-                   
-                }
-            })
-            .then((response) => {
-                this.project = response.data.results;
-            })
+        getProject() {
+            axios.get(this.urlAddress + `/api/projects/${this.$route.params.id}`, {})
 
-            .catch(function (error) {
-                console.log(error);
-            })
+                .then((response) => {
+                    console.log(response.data.results)
+                    this.project = response.data.results;
+                    console.warn(response);
+                })
+                .catch(function (error) {
+                    console.warn(error);
+                });
         }
     },
     created() {
-        this.getProject();
-    }
-}
+    this.getProject();
+    },
+};
 </script>
 
 <template>
-    
-
-    <div class="container d-flex flex-wrap p-3">
-        <div class="row g-3">
-            <AppProject :project="project"/>     
+    <div class="container p-3">
+        <div class="row d-flex justify-content-center">
+            <AppProject :project="project" :isShow="true" :isHover="false"/>
         </div>
     </div>
 </template>
 
+
 <style lang="scss" scoped>
-@use '../styles/general.scss' as * ;
 @use '../styles/partials/variables' as * ;
-
-
 
 </style>
